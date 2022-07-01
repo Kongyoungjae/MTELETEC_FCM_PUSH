6559ea164af04945bfdee5a7f42033a5a2970808
Kongyoungjae <youngjae4679@gmail.com>
2022-07-01 17:00:26 +0900

    main 하나만 실행 가능

--- FcmPush/src/fcmpush/main/Main.java
+++ FcmPush/src/fcmpush/main/Main.java
@@ -1,21 +1,17 @@
 package fcmpush.main;
 
 import java.io.IOException;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.SocketException;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.google.firebase.internal.FirebaseService;
-
-import fcmpush.config.DataBaseConfig;
-import fcmpush.config.FireBaseConfig;
-import fcmpush.enumeration.FireBaseEnum;
-import fcmpush.enumeration.IntervalEnum;
 import fcmpush.exception.ErrorHandler;
 import fcmpush.exception.ExceptionHandler;
-import fcmpush.service.FireBasePushService;
 import fcmpush.thread.FireBasePushThread;
 import fcmpush.thread.PushGroupThread;
 import fcmpush.util.DateUtil;
@@ -23,54 +19,54 @@ import fcmpush.util.DateUtil;
 public class Main {
 
 	private static final Logger logger = LogManager.getLogger(Main.class);
-	public static void main(String[] args) {
-		
+	private static DatagramSocket isRun;
+
+	public static void main(String[] args) throws SocketException {
+
+		isRun = new DatagramSocket(31997);
+
+		System.out.println("run 했어요");
+
 		logger.info("@@@MAIN실행!!@@@");
 		run();
 	}
 
-	public static void run () {
-		
+	public static void run() {
+
 		try {
-			DataBaseConfig.init();
-			FireBaseConfig.init();
-			
-//			// 수신그룹 만들기 스레드
-			Timer receiveGroupTimer = new Timer();
-			TimerTask makeReceiveGroupTask = new TimerTask() {			
+			// 수신그룹 만들기 스레드
+//			Timer receiveGroupTimer = new Timer();
+//			TimerTask makeReceiveGroupTask = new TimerTask() {			
+//				@Override
+//				public void run() {
+//					MakeGroupThread groupThread = new MakeGroupThread();
+//					groupThread.run();
+//				}			
+//			};
+//			receiveGroupTimer.scheduleAtFixedRate(makeReceiveGroupTask, DateUtil.todayFourAm().getTime() , FireBaseIntervalEnum.All_GROUP_CREATE_INTERVAL.getInterval());
+
+			// 푸쉬 스레드
+			Timer pushTimer = new Timer();
+			TimerTask pushTask = new TimerTask() {
 				@Override
 				public void run() {
-					PushGroupThread groupThread = new PushGroupThread();
-					groupThread.run();
-				}			
+
+					FireBasePushThread pushThread = new FireBasePushThread();
+					pushThread.run();
+
+				}
 			};
-			receiveGroupTimer.scheduleAtFixedRate(makeReceiveGroupTask, DateUtil.todayFourAm() , IntervalEnum.All_GROUP_CREATE_INTERVAL.getInterval());
-//			receiveGroupTimer.scheduleAtFixedRate(makeReceiveGroupTask, DateUtil.tomorrowFourAm() , IntervalEnum.All_GROUP_CREATE_INTERVAL.getInterval());
-			
-			//푸쉬 스레드
-//			Timer pushTimer = new Timer();
-//			TimerTask pushTask = new TimerTask() {			
-//				@Override
-//				public void run() {					
-//					FireBasePushThread pushThread = new FireBasePushThread();
-//					pushThread.run();
-//
-//				}
-//			};
-//			pushTimer.scheduleAtFixedRate(pushTask, 0 , 1000 * 10 ); // IntervalEnum.PUSH_CHECK_INTERVAL.getInterval()
+			pushTimer.scheduleAtFixedRate(pushTask, 0, 1000);
 			Thread.sleep(1000);
-		}		
-		catch(RuntimeException e) {	
-			throw new ExceptionHandler(e);	
-		} 
-		catch (InterruptedException e) {
+
+		}
+
+		catch (RuntimeException e) {
 			throw new ExceptionHandler(e);
-		} 	
-		catch(Error e) {	
+		} catch (InterruptedException e) {
+			throw new ExceptionHandler(e);
+		} catch (Error e) {
 			throw new ErrorHandler(e);
-		} catch (IOException e) {
-			throw new ExceptionHandler(e);
 		}
 	}
 }
-
